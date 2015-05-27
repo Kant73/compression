cfbe80afc748d92487b66a765aabdb39fda2ae10
Quentin Dunand <quentindunand@gmail.com>
2015-05-27 15:44:16 +0200

    Début modfification Compression

---
+++ include/compression.h
@@ -0,0 +1,17 @@
+/*
+	Projet CPS - Compression LZW
+	Adrien DELAPORTE - Quentin DUNAND - Elsa NAVARRO - Florian POPEK
+	compression.h
+*/
+
+
+
+/**
+ * [encode description]
+ * @param
+ * @param
+ * @param
+ */
+void encode(file* E, file* S, DICO);
+
+
