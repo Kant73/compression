6e9b09f5fa8f9e40fa7c6fa6222b583421a4a39a
FlorianPO <florian.popek@gmail.com>
2015-05-27 14:34:54 +0200

    Makefile mis à jour + espace de travail dans Main.c

--- src/Main.c
+++ src/Main.c
@@ -8,6 +8,28 @@
 
 int main(int argc, char* argv[])
 {
-	//TODO
+	#ifdef DEBUG
+		printf("<DEBUG MODE ENABLED>\n");
+	#endif
+	
+	FILE* file;
+	file = fopen(/*nom du fichier*/, "r");
+	
+	
+	if (file == NULL)
+	{
+		printf("Impossible d'ouvrir le fichier...\n");
+		return 1;
+	}
+	
+	while (!feof(file)) //Lire les mots
+	{
+		
+	}
+	
+	fclose(file);
+	
+	//Libérer la mémoire
+	
 	return 0;
 }
