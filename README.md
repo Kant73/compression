1cd653694daf23ab782aac4e42a131281d44a7b9
Quentin Dunand <quentindunand@gmail.com>
2015-05-28 21:10:52 +0200

    Test de la compression - Makefile prévu à cet effet - MAJ du README

--- README.md
+++ README.md
@@ -3,18 +3,14 @@
 
 #TODO:
 ###Dictionnaire:
-- Initialiser_mot
-- Chercher_mot
-- Get_Code
-- Get_Chaine
+- Vérifier fonction chercher_code_dico
+- Vérifier fonction inserer_dico
 - Faire un tableau de correspondance code/chaine pour la decompression (penser à une structure de donnée)
 
 
 ###Compression:
-- Finir l'algo
 - Tester l'algo
 - Commenter le code
-- Ecrire un mot codé dans un fichier depuis une variable de type type_mot retourné par la fonction paquet8
 
 ###Decompression:
 - Faire l'algo
--- src/compression.c
+++ src/compression.c
@@ -14,47 +14,68 @@
  * [encode description]
  * @param E 	Le fichier qu'on veut encoder
  * @param S 	Le fichier qui contient le fichier encoder
- * @param dic 	Le dictionnaire
+ * @param dico 	Le dictionnaire
  */
-void encode(FILE* E, FILE* S, type_dico* dic)
+void encode(FILE* E, FILE* S, type_dico* dico)
 {
 	uint8_t a;				//Pour stocker un octet (l'octet courant)
 
 	type_mot mot;			//Notre chaine d'octet
 	type_mot mot_concat;	//Contient la chaine d'octet concaténé avec l'octet courant
 	int code = -1;			//Contient le code d'une chaine d'octet
+	int code_mot = -1;
 	int taille_code_max = 9;
 	//initialisation du Dico
-	initialiser_dico(dic);
+	initialiser_dico(dico);
 
 	//A modifier : on ouvrira les fichier dans le main
 	E = fopen("entree.txt", "r");
 	S = fopen("sortie.txt", "w");
 
-	init_mot(&mot, (uint8_t)fgetc(E));	//On initialise le mot avec le premier octet de E
+	init_mot(&mot, fgetc(E));	//On initialise le mot avec le premier octet de E
 										//Correspond à w <- [1er octet de E]
 	
-	mot_concat = mot;
+	
 	if (E != NULL)						//On teste si E n'est pas null ce qui signifierai 
 	{									//que le fichier n'est pas ouvert
 		while(!feof(E))					//Tant qu'on atteint pas la fin de fichier
 		{
-			a = (uint8_t)fgetc(E);				//On stocke dans a le caractère courant
-			insere_queue_mot(mot_concat, a);	//On insère en queue de notre chaine d'octet l'octet courant
-			if ( (code = chercher_mot(mot_concat)) >= 0)	//On stocke dans code le code du mot concatener
+			mot_concat = mot;
+			a = fgetc(E);				//On stocke dans a le caractère courant
+			inserer_queue_mot(&mot_concat, a);	//On insère en queue de notre chaine d'octet l'octet courant
+			/*type_mot* tmp = &mot_concat;
+			while(tmp != NULL)
+			{
+				printf("%d\n", tmp->lettre);
+				tmp = tmp->suivant;
+			}
+			*/
+			//printf("mot_concat.lettre %d\n", mot_concat.lettre);
+			//printf("mot_concat.suivant->lettre %d\n", mot_concat.suivant->lettre);
+			code = chercher_code_dico(&mot_concat, dico);
+			//printf("Code mot_concat %d\n", code);
+			if (code >= 0)	//On stocke dans code le code du mot concatener
 			{
 				mot = mot_concat;
 			}
 			else 
 			{
-				//paquet8_ecrire(code, taille_code_max, S); 	//ecrire dans le fichier le code du mot
-				inserer_dico(&mot_concat, dic, &taille_code_max, S);
-				liberer_mot(&mot);
+				code_mot = chercher_code_dico(&mot, dico);
+				printf("Code_mot %d\n", code_mot);
+				paquet8_ecrire(code_mot, taille_code_max, S); 	//ecrire dans le fichier le code du mot
+				inserer_dico(&mot_concat, dico, &taille_code_max, S);
+				liberer_mot(mot.suivant);
+				liberer_mot(mot_concat.suivant);
 				init_mot(&mot, a);
+				printf("mot.lettre %d\n", mot.lettre);
+				//printf("Mot.lettre: %d\n", mot.lettre);
+				//printf("Pointeur sur mot suivant : %d\n", mot.suivant);
+
 			}
 		}
-		fprintf(S, "%d", code);	
 	}
-	liberer_mot(&mot);
-	liberer_mot(&mot_concat);
+	//liberer_mot(mot.suivant);
+	//liberer_mot(mot_concat.suivant);
+	//liberer_dico(dico);
+	//afficher_dico(dico);
 }
