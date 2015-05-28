0ce5d0ce2ffc01e4cfdf3b4e467d27ccd6e7f134
Elsa Navarro <elsa0navarro@gmail.com>
2015-05-28 17:51:20 +0200

    Decompression compile, à tester

--- src/decompression.c
+++ src/decompression.c
@@ -11,39 +11,43 @@
 #include "../include/decompression.h"
 	//lecture et écriture binaire
 	//lecture en mode "buffer"
+type_mot* chercher_chaine_dico(int code, type_dico* dico);
 
-void decode(FILE* E, FILE* S, type_dico* dico){/*
+void decode(FILE* E, FILE* S, type_dico* dico){
 	int i,j; //index dans D
-	type_mot* mot,mot2; // chaines d'octets
+	type_mot mot,mot2; // chaines d'octets
 	uint8_t a;
+	int taille_codes;
 	initialiser_dico(dico);
 
 	E = fopen("entree.txt", "r");
 	S = fopen("sortie.txt", "w");
 
-	if ((E != NULL)&&(F !=NULL))					//On teste si les fichiers sont bien ouverts 
+	if ((E != NULL)&&(S !=NULL))					//On teste si les fichiers sont bien ouverts 
 	{
-		i = recupereNextCodeFile(E); 		//récupère un premier code
-		a = getChaine(i, dico).valeur; //a chaine cherchée, i code de la chaine ds le dico
+
+		i = paquet8_lire(E, &taille_codes); 		//récupère un premier code
+		a = chercher_chaine_dico(i, dico)->lettre; //a chaine cherchée, i code de la chaine ds le dico
 		mot.lettre =a;
 		ecrire_lettre(S, mot); 			//On écrit le mot sur le fichier de sortie
 
 		while(!feof(E))					//Tant qu'on atteint pas la fin de fichier
 			{
-				j=recupereNextCodeFile(E);
-				if(chercher_code_dico(mot, dico)==-1){
-					mot2 = getChaine(i, dico);
+				j=paquet8_lire(E, &taille_codes); //on récupère l'entier suivant
+
+				if(chercher_chaine_dico(i, dico)!=NULL){
+					mot2 = *chercher_chaine_dico(i, dico);
 					insereQueue(&mot2, a);
 				}
 				else{
-					mot2 = getChaine(j, dico);
+					mot2 = *chercher_chaine_dico(j, dico);
 				}
 				ecrire_lettre(S, mot2);
 				a = mot2.lettre;
-				insereQueue(&mot, a);
-				inserer_dico(mot, dico);
+				insere_queue(&mot, a);
+				inserer_dico(&mot, dico, &taille_codes, S);
 				i=j;
-				w= getChaine(i, dico);
+				mot = *chercher_chaine_dico(i, dico);
 			}
-	}*/
+	}
 }
