fee3ea99423897366725b86563ba3ce16176ddc2
Elsa Navarro <elsa0navarro@gmail.com>
2015-05-28 15:30:33 +0200

    Mise à jour de encode avec dictionnaire.c

--- src/compression.c
+++ src/compression.c
@@ -17,13 +17,13 @@
  * @param dic 	Le dictionnaire
  */
 void encode(FILE* E, FILE* S, type_dico* dic)
-{/*
+{
 	uint8_t a;				//Pour stocker un octet (l'octet courant)
 
 	type_mot mot;			//Notre chaine d'octet
 	type_mot mot_concat;	//Contient la chaine d'octet concaténé avec l'octet courant
 	int code = -1;			//Contient le code d'une chaine d'octet
-	
+	int taille = 0;
 	//initialisation du Dico
 	initialiser_dico(dic);
 
@@ -31,7 +31,7 @@ void encode(FILE* E, FILE* S, type_dico* dic)
 	E = fopen("entree.txt", "r");
 	S = fopen("sortie.txt", "w");
 
-	initMot(mot, (uint8_t)fgetc(E));	//On initialise le mot avec le premier octet de E
+	init_mot(mot, (uint8_t)fgetc(E));	//On initialise le mot avec le premier octet de E
 										//Correspond à w <- [1er octet de E]
 
 	
@@ -40,22 +40,23 @@ void encode(FILE* E, FILE* S, type_dico* dic)
 	{									//que le fichier n'est pas ouvert
 		while(!feof(E))					//Tant qu'on atteint pas la fin de fichier
 		{
-			a = fgetc(E);				//On stock dans a le caractère courant
-			insereQueue(mot_concat, a);	//On insère en queue de notre chaine d'octet l'octet courant
+			a = (uint8_t)fgetc(E);				//On stock dans a le caractère courant
+			insere_queue_mot(mot_concat, a);	//On insère en queue de notre chaine d'octet l'octet courant
 			if ( (code = chercher_mot(mot_concat)) >= 0)	//On stock dans code le code du mot concatener
 			{
 				mot = mot_concat;
 			}
 			else 
 			{
-				fprintf(S, "%d", code);
-				inserer_dico(mot_concat, dic);
-				liberer_mot(mot);
+				taille = nbr_bit(code);
+				//paquet8_ecrire(code, taille, S); 	//ecrire dans le fichier le code du mot
+				inserer_dico(&mot_concat, dic);
+				liberer_mot(&mot);
 				init_mot(mot, a);
 			}
 		}
 		fprintf(S, "%d", code);	
 	}
-	liberer_mot(mot);
-	liberer_mot(mot_concat);*/
+	liberer_mot(&mot);
+	liberer_mot(&mot_concat);
 }
