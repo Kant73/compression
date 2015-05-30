4fb48aa35ceca43e51af5656f666e5b9b95190c9
Quentin Dunand <quentindunand@gmail.com>
2015-05-30 15:32:35 +0200

    compression.c commenté et cleaner !

--- src/compression.c
+++ src/compression.c
@@ -7,55 +7,55 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "../include/dictionnaire.h"
-#include "../include/compression.h"
+#include "dictionnaire.h"
+#include "compression.h"
 
 /**
- * [encode description]
+ * Permet d'encoder le fichier E dans le fichier S en utilisant le dictionnaire dico
  * @param E 	Le fichier qu'on veut encoder
  * @param S 	Le fichier qui contient le fichier encoder
  * @param dico 	Le dictionnaire
  */
 void encode(FILE* E, FILE* S, type_dico* dico)
 {
-	type_mot mot, mot_concat;	//Notre chaine d'octet , //Contient la chaine d'octet concaténé avec l'octet courant
+	type_mot mot, mot_concat;	//mot : Notre chaine d'octet; mot_concat: Contient la chaine d'octet concaténé avec l'octet courant
 	mot_concat.suivant = NULL;
 
 	int taille_code_max = 9;
 	
-	initialiser_dico(dico, COMP); //initialisation du Dico
+	initialiser_dico(dico, COMP); //initialisation du Dico (paramètre COMP pour la compression)
 
-	//A modifier : on ouvrira les fichier dans le main
+	//A modifier eventuellement : on ouvrira les fichiers dans le main
 	E = fopen("entree.txt", "r");
 	S = fopen("sortie.txt", "w");
 	
 	if (E == NULL)
-		printf("Impossible d'ouvir le fichier...\n");
+		printf("Impossible d'ouvir le fichier d'entrée...\n");
 
 	init_mot(&mot, fgetc(E));	//On initialise le mot avec le premier octet de E
-										//Correspond à w <- [1er octet de E]
+								//Correspond à w <- [1er octet de E]
 
-	if (E != NULL)						//On teste si E n'est pas null ce qui signifierai 
-	{									//que le fichier n'est pas ouvert
-		while(!feof(E))					//Tant qu'on atteint pas la fin de fichier
+	if (E != NULL)				//On teste si E n'est pas null ce qui signifierai 
+	{							//que le fichier n'est pas ouvert
+		while(!feof(E))			//Tant qu'on atteint pas la fin de fichier
 		{
-			affecter_mot(&mot_concat, &mot);
+			affecter_mot(&mot_concat, &mot);	//mot_concat := mot
 
-			uint8_t a = fgetc(E);				//On stocke dans a le caractère courant
+			uint8_t a = fgetc(E);				//On stocke dans a l'octet courant
 			inserer_queue_mot(&mot_concat, a);	//On insère en queue de notre chaine d'octet l'octet courant
 
 			#ifdef DEBUG
 				printf("Caractère récupéré du fichier: %c\t%s\n", a, to_binaire(a));
 			#endif
 			
-			int code = chercher_code_dico(&mot_concat, dico);
-			if (code >= 0)
-				inserer_queue_mot(&mot, a); //On stocke dans code le code du mot concatené
+			int code = chercher_code_dico(&mot_concat, dico);	//code = -1 si mot_concat (w.a) non trouvé dans le dico
+			if (code >= 0)										//Si mot_concat est dans le dico, code = code de mot_concat du dico
+				inserer_queue_mot(&mot, a); //Equivalent à mot := mot_courant (w <- w.a)
 			else 
 			{
 				int code_mot = chercher_code_dico(&mot, dico);
-				paquet8_ecrire(code_mot, taille_code_max, S); 	//ecrire dans le fichier le code du mot
-				inserer_dico(&mot_concat, dico, &taille_code_max, S);
+				paquet8_ecrire(code_mot, taille_code_max, S); 			//On ecrit dans le fichier le code du mot (w)
+				inserer_dico(&mot_concat, dico, &taille_code_max, S);	//Et on insère le mot_concat (w.a) dans le dico
 				
 				if (mot.suivant != NULL)
 					liberer_mot(mot.suivant);
@@ -68,9 +68,10 @@ void encode(FILE* E, FILE* S, type_dico* dico)
 			}
 		}
 	}
-	paquet8_ecrire(256, taille_code_max, S); //Fin de fichier -> 256
-	paquet8_ecrire(0, 7, S); //Compléter avec des 0
+	paquet8_ecrire(256, taille_code_max, S);	//Fin de fichier -> 256, on écris le fichier pour signaler la fin de fichier
+	paquet8_ecrire(0, 7, S); 					//On compléter le fichier avec des 0
 	
+	//On libère tout à la fin de la compression du fichier
 	if (mot.suivant != NULL)
 		liberer_mot(mot.suivant);
 	if (mot_concat.suivant != NULL)
